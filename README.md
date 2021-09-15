b79df480502ded49a9c79b751d04e41c4e2de1c5
Marcelo Araujo <araujobsdport@gmail.com>
2021-09-15 11:35:35 +0800

    Release v1.0.1 (#1)
    * Update README
    
    * Add src code
    
    * Add systemd script
    
    Co-authored-by: Marcelo Araujo <marcelo.araujo@gandi.net>

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# prometheus-linux-nfsdv4-exporter
+# prometheus-linux-nfsdv4-exporter - prometheus exporter for NFSv4 server. 
+
+<b>prometheus-linux-nfsdv4-exporter</b> It is a prometheus-exporter that reports basic metrics of NFSv4 clients from the server side. Since Linux Kernel 5.3, NFSv4 mount points have information reported inside the /proc/fs/nfsd/clients/, it is possible to retrieve some limited information of clients that are using NFSv4. 
+
+### Prometheus metrics:
+* nfs_iobytes_read Total of bytes read
+* nfs_iobytes_write Total of bytes write
+* nfs_network_connections Total amount of network connections
+* nfs_network_netcount Total amount of packets
+* nfs_network_tcpcount Total amount of TCP packets
+* nfs_network_udpcount Total amount of UDP packets
+* nfs_reply_cache_hits Number of cache hits
+* nfs_reply_cache_misses Number of cache misses
+* nfs_reply_cache_nocache Number of nocache
+* <b>nfsv4_op_deleg_per_client Number of deleg operations per NFSv4 client</b>
+* <b>nfsv4_op_layout_per_client Number of layout operations per NFSv4 client</b>
+* <b>nfsv4_op_lock_per_client Number of lock operations per NFSv4 client</b>
+* <b>nfsv4_op_open_per_client Number of open operations per NFSv4 client</b>
+* <b>number_of_nfsv4_clients Number of NFSv4 clients</b>
+ 
+### Build the project:
+* Release: <b>```cargo build --release```</b>
+
+### Debian package:
+* First install: <b>```cargo install cargo-deb```</b>
+* Generate the debian package: <b>```cargo deb -v```</b>
+
+### Contributing:
+<a href="https://github.com/Gandi/prometheus-linux-nfsdv4-exporter/graphs/contributors">
+  <img src="https://contributors-img.web.app/image?repo=Gandi/prometheus-linux-nfsdv4-exporter" />
+</a>
+
+### License:
+
+The project is made available under the BSD 2-Clause license. See the `LICENSE` file for more information.
---
+++ src/utils/helper.rs
@@ -0,0 +1,60 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2021, Gandi S.A.S.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+extern crate nix;
+extern crate semver;
+use nix::sys::utsname::*;
+use semver::Version;
+
+use std::path::Path;
+
+// NFS
+pub const PROC_NFSDV4: &'static str = "/proc/fs/nfsd/";
+pub const PROC_RPC: &'static str = "/proc/net/rpc/";
+
+// Linux kernel
+const LINUX_MINIMAL_VERSION: &'static str = "5.3.0";
+
+#[inline]
+pub fn is_kernel_compatible() -> bool {
+    let sysinfo = uname();
+    let _kernel_version: Vec<&str> = sysinfo.release().split('-').collect();
+    let kernel_version = _kernel_version[0];
+
+    if Version::parse(kernel_version).unwrap() >= Version::parse(LINUX_MINIMAL_VERSION).unwrap() {
+        return true;
+    }
+    false
+}
+
+#[inline]
+pub fn path_exists(path: &str) -> bool {
+    Path::new(path).exists()
+}
